89db712a2e691fe09bf592c4dc63d83eaf659d22
aemann01 <allison.e.mann@gmail.com>
2022-02-11 16:38:28 -0500

    fixing plyr error

--- 2022-HIV_oral_microbiome/01-read_processing/fix_taxonomy.py
+++ 2022-HIV_oral_microbiome/01-read_processing/fix_taxonomy.py
@@ -1,4 +1,4 @@
-''' Adds missing fields to list of taxonomy strings. Use: python fix_taxonomy_L7.py my_taxa.txt > new_taxa.txt '''
+''' Adds missing fields to list of taxonomy strings. Use: python fix_taxonomy.py my_taxa.txt > new_taxa.txt '''
 
 import sys
 
@@ -6,8 +6,8 @@ with open(sys.argv[1], "r") as f:
 	for line in f:
 		x = line.rstrip("\n")
 		y = x.count(";")
-		if y < 6:
-			add = 6 - y
+		if y < 10:
+			add = 10 - y
 			new = (";" + x.split(";")[-1] + "_unknown") * add
 			print(x + new)
 		else:
